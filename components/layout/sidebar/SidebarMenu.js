74ff75b6e3b9b1a46eef34af9995fc80a088e54d
Cindy <cdyy2400@gmail.com>
2023-02-18 16:39:19 +0700

    active sidebar style

--- components/layout/sidebar/SidebarMenu.js
+++ components/layout/sidebar/SidebarMenu.js
@@ -1,9 +1,19 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styles from "../../../styles/SidebarMenu.module.scss";
 
 const SidebarMenu = ({ menuName, linkTo }) => {
+  const router = useRouter();
+
   return (
-    <Link href={linkTo} className={styles["sidebar-menu-button"]}>
+    <Link
+      href={linkTo}
+      className={
+        router.pathname === linkTo
+          ? styles["sidebar-menu-button-active"]
+          : styles["sidebar-menu-button"]
+      }
+    >
       {menuName}
     </Link>
   );
